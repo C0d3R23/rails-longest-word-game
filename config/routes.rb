30d9c55254ba0aacca41f14f99559f6141b52aa6
Grzegorz Walasek <mail969@wp.pl>
2018-11-26 16:44:40 +0000

    word games

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
-Rails.application.routes.draw do
+#Rails.application.routes.draw do
+  #get 'games/new'
+  #get 'games/score'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#end
+
+Rails.application.routes.draw do
+
+  get '/new', to: "games#new"
+  post '/score', to: "games#score"
+
 end
